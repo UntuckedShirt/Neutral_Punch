7b0133421d16f41bdcdf507566c73c3eaab4e747
Nathan Miller <natem31190@gmail.com>
2021-06-04 19:28:37 -0400

    Here we go

---
+++ db/migrate/20210604225018_create_jabs.rb
@@ -0,0 +1,9 @@
+class CreateJabs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :jabs do |t|
+      t.string :title
+      t.text :description
+      t.timestamps
+    end
+  end
+end
